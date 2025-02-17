e0612ae006e94fc0ccd0ae0149414010dd75e674
Your Name <you@example.com>
2025-02-16 21:24:11 -0500

    full

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import {Header} from "./Components/Header";
+import {Main} from "./Components/Main";
+import {Text} from "./Components/Section";
+import {Map} from "./Components/Map";
+import {Footer} from "./Components/footer";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Text/>
+    <Map/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
